281e3c1638fcc27e13931b3accfacfe7a16095fd
encima <encima@gmail.com>
2018-08-13 12:16:19 +0200

    added swagger file option to config

--- config/test.py
+++ config/test.py
@@ -2,6 +2,7 @@ DB_URI = 'postgresql://pybossa:tester@localhost/cs'
 PORT = 8080
 DEBUG = True
 SWAGGER_DIR='swagger/'
+SWAGGER_FILE='swagger.json'
 SECRET_KEY='SUPES_SECRET987'
 TEST_USER = 'test@test.com'
 TEST_PWD = 'test'
