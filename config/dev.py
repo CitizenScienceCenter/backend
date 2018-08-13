70dc21124efa6db135060b4233dbef7e482571b9
encima <encima@gmail.com>
2018-08-13 12:59:31 +0200

    wrong bloody extension

--- config/dev.py
+++ config/dev.py
@@ -2,5 +2,5 @@ DB_URI = 'postgresql://pybossa:tester@localhost/cs'
 PORT = 8080
 DEBUG = True
 SWAGGER_DIR='swagger/'
-SWAGGER_FILE='swagger.json'
+SWAGGER_FILE='swagger.yaml'
 SECRET_KEY='SUPES_SECRET987'
