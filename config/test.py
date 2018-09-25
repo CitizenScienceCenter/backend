8cfb2b0c42551b5e6a1164507f1c1fe493f79f42
encima <encima@gmail.com>
2018-09-25 11:51:03 +0200

    added test file

---
+++ config/test.py
@@ -0,0 +1,9 @@
+DB_URI = 'postgresql://pybossa:tester@localhost/cs'
+PORT = 8080
+DEBUG = True
+SWAGGER_DIR='swagger/'
+SWAGGER_FILE='swagger.yaml'
+SECRET_KEY='SUPES_SECRET987'
+TEST_USER = 'test@test.com'
+TEST_PWD = 'test'
+TESTING = True
