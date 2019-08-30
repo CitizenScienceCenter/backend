f013554785acaf7bdc2da737d80092424a73c066
encima <encima@gmail.com>
2019-08-30 11:12:03 +0200

    added session to orm handler in app init

--- db/orm_handler.py
+++ db/orm_handler.py
@@ -18,23 +18,25 @@ import os
 
 Base = declarative_base(cls=CSBase)
 db_instance = None
-
 db_session = None
 
 def db_init():
     global db_instance
+    global db_session
     load_dotenv()
     persist = True
     if db_instance:
         return db_instance
     db_uri = os.getenv('DB_URI')
     engine = create_engine(db_uri, pool_size=25, max_overflow=10)
-    db_session = scoped_session(
+    session = scoped_session(
         sessionmaker(autocommit=False, autoflush=False, bind=engine)
     )
-    Base.query = db_session.query_property()
+    Base.query = session.query_property()
     if not persist:
         Base.metadata.drop_all(engine)
     Base.metadata.create_all(bind=engine)
-    # db_instance = db_session
-    # return db_instance
+    db_session = session
+    # return session
+
+# db_session = db_init()
