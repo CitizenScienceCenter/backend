d099287ba2bc401b3a315f28a501bd6a3b07233d
encima <encima@gmail.com>
2019-08-30 10:23:52 +0200

    created scoped session on startup

--- db/orm_handler.py
+++ db/orm_handler.py
@@ -11,7 +11,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import Flask, current_app
 
-session = flask_scoped_session(session_factory, app)
+from flask_sqlalchemy_session import flask_scoped_session
 
 import datetime
 from db.cs_base import CSBase
