c9bd98eb9915ed9fa1df34b919b19a270369a577
Chris Gwilliams <encima@gmail.com>
2019-12-19 10:16:13 +0200

    added invite endpoints

--- api/member/custom.py
+++ api/member/custom.py
@@ -3,7 +3,7 @@ from flask import abort, request
 from pony.flask import db_session
 from sqlalchemy.orm import joinedload, lazyload
 from pony.orm import commit
-from db import Project, Submission, Task, User, utils, Member, Role
+from db import Project, Submission, Task, User, utils, Member, Role, ProjectGroup
 from decorators import access_checks
 from middleware.response_handler import ResponseHandler
 
@@ -19,6 +19,22 @@ def invite_member(pid, body):
         try:
             commit()
             return ResponseHandler(200, "User added to project").send()
+        except Exception as e:
+            abort(500, e)
+    else:
+        abort(404, "User not found")
+
+@db_session
+@access_checks.ensure_owner(ProjectGroup)
+def invite_member_group(gid, body):
+    r = Role.get(name=body["role"])
+    u = User.get(username=body["username"])
+    p = ProjectGroup.get(id=gid)
+    if p and u and r:
+        m = Member(user_id=u, project_group_id=p, role=r)
+        try:
+            commit()
+            return ResponseHandler(200, "User added to project group").send()
         except Exception as e:
             abort(500, e)
     else:
